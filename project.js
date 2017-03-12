de3611e5a036d76183a0ea5dd94d2e205a810c00
iwonanna <iwona.biernat90@gmail.com>
2017-03-12 23:31:37 +0000

    added social to every sub-page

--- project.js
+++ project.js
@@ -1,1 +1,12 @@
-console.log('Hello');
+// Activate Carousel
+$("#myCarousel").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+    $("#myCarousel").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+});
