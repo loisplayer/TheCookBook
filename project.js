c2f01f0524045175f363368f3a0466b6639ea282
loisplayer <lkplayer@hotmail.co.uk>
2017-03-13 12:48:04 +0000

    fix carousel on whats on page. Altered alert box to be in separate js file

--- project.js
+++ project.js
@@ -1,12 +1,3 @@
-// Activate Carousel
-$("#myCarousel").carousel();
-
-// Enable Carousel Indicators
-$(".item").click(function(){
-    $("#myCarousel").carousel(1);
-});
-
-// Enable Carousel Controls
-$(".left").click(function(){
-    $("#myCarousel").carousel("prev");
-});
+function subscribe() {
+    alert("Thank you for registering your email!");
+}
